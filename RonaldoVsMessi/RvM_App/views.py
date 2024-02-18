5035f09e16dc180ab39a221a64243c174cfe5431
AmenoAmena <ardacavusoglu666@gmail.com>
2024-02-18 21:47:30 +0300

    final project

--- RonaldoVsMessi/RvM_App/views.py
+++ RonaldoVsMessi/RvM_App/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from .models import Ronaldo,Messi
+from .models import Ronaldo, Messi
 
-# Create your views here.
 def index(request):
-    ronaldo_score = Ronaldo.objects.first().score_r
-    messi_score = Messi.objects.first().score_m
+    ronaldo_instance, created_r = Ronaldo.objects.get_or_create(pk=1)
+    ronaldo_score = ronaldo_instance.score_r
+
+    messi_instance, created_m = Messi.objects.get_or_create(pk=1)
+    messi_score = messi_instance.score_m
+
+    if request.method == 'POST':
+        player = request.POST.get('player')
+        if player == 'ronaldo':
+            ronaldo_instance.increase_score_r()
+        elif player == 'messi':
+            messi_instance.increase_score_m()
+
     return render(request, 'RvM_App/index.html', {
         'score_m': messi_score,
         'score_r': ronaldo_score
     })
+
